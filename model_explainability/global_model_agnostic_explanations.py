71d53194ff5dd559b8e5c6920b2521d792967ab8
FabioLddo <loddo9915@gmail.com>
2022-11-29 16:20:26 +0100

    Setup for global explanations

---
+++ model_explainability/global_model_agnostic_explanations.py
@@ -0,0 +1,18 @@
+# Libraries:
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from config import *
+
+
+
+# Data manipulation:
+from pathlib import Path
+
+# Splitting the data:
+from modelling.train_test_validation_split import split_data
+
+
+# Global variables:
+data_path = Path("..", "data")
